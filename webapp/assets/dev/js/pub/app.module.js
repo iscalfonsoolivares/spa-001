07c40374bd3ed6c1ac5f53a9606d002416af19b0
Alfonso Olivares <contacto@iscalfonsoolivares.com>
2015-11-21 10:18:22 -0600

    adding router

--- webapp/assets/dev/js/pub/app.module.js
+++ webapp/assets/dev/js/pub/app.module.js
@@ -3,7 +3,29 @@
   'use strict';
   
   angular
-      .module('app', ['ngRoute', 'HelloController']);
+      .module('app', ['ngRoute'])
+      .config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'assets/dev/js/pub/hello/hello.html',
+                controller  : 'HelloController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'assets/dev/js/pub/hello/hello.html',
+                controller  : 'HelloController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'assets/dev/js/pub/hello/hello.html',
+                controller  : 'HelloController'
+            });
+    });
+  
   
 
   /*
