ba540b2e9c0748224466e7c77fcf45a28e675f00
Alfonso Olivares <contacto@iscalfonsoolivares.com>
2015-11-21 06:12:56 -0600

    controller name change

--- webapp/assets/dev/js/pub/app.module.js
+++ webapp/assets/dev/js/pub/app.module.js
@@ -1,8 +1,29 @@
 (function() {
   
   'use strict';
-
+  
   angular
-      .module('app', ['ngRoute']);
+      .module('app', ['ngRoute', 'HelloController']);
+  
+
+  /*
+  angular
+      .module('app', ['ngRoute'])  
+      .config(['$routeProvider',
+      function($routeProvider) {
+        $routeProvider.
+          when('/AddNewOrder', {
+            templateUrl: 'templates/add_order.html',
+            controller: 'AddOrderController'
+        }).
+          when('/ShowOrders', {
+            templateUrl: 'templates/show_orders.html',
+            controller: 'ShowOrdersController'
+          }).
+          otherwise({
+            redirectTo: '/AddNewOrder'
+          });
+    }]);
+  */  
   
 })();  
